01521b9196bdecc25b33aa26a6fdaef1eb538aba
Pravesh Srivastava <papsri.2015@gmail.com>
2026-05-09 02:09:24 +0530

    data loading and ingestion code added

---
+++ src/rag/ingestion.py
@@ -0,0 +1,45 @@
+import pandas as pd
+
+from langchain_chroma import Chroma
+from langchain_core.documents import Document
+
+from src.data.data_loading import load_efficiently
+from src.embedding.embedding_model import get_model
+
+
+def to_documents(df: pd.DataFrame):
+    
+    df = df.copy()
+    df['text'] = df['title'] + "\n\n" + df['abstract']
+
+    result = df[ df['id', 'text', 'categories', 'update_date', 'license' ] ]
+
+    docs = []
+
+    for row in result.itertuples():
+        docs.append(
+            Document(page_content=row.text,
+                     metadata={
+                         'id': row.id,
+                         'category': row.categories,
+                         'update_date': row.update_date,
+                         'license': row.license
+                     })
+        )
+
+    return docs
+
+
+def create_vector_store(storepath: str, filepath: str, embedding_model):
+    
+    for chunk in load_efficiently(filepath=filepath):
+        docs = to_documents(chunk)
+
+        vector_store = Chroma(
+            embedding_function=embedding_model,
+            persist_directory=storepath,
+            collection_name="ai_ml"
+        )
+        vector_store.add_documents(docs)
+    
+    return vector_store
