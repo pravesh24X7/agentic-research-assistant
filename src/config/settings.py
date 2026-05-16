d73ed4055bf2e68844d0dbca95628cceecf47a7a
Pravesh Srivastava <papsri.2015@gmail.com>
2026-05-17 03:34:00 +0530

    temperature variable added in settings.py

--- src/config/settings.py
+++ src/config/settings.py
@@ -13,3 +13,4 @@ EMBEDDING_MODEL_NAME="sentence-transformers/all-MiniLM-L6-v2"
 LLM_MODEL="meta-llama/llama-4-scout-17b-16e-instruct"
 DB_NAME="db/research.db"
 SAVE_PROMPT_TO = os.path.join("prompts")
+TEMPERATURE = 0.5
